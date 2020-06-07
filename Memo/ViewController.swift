9bfc23dbbd173708399525dbebafc7ba5b415a31
Michelle Lau <mdeniselau@gmail.com>
2020-06-07 10:57:49 +0900

    first commit

---
+++ Memo/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Memo
+//
+//  Created by Michelle Lau on 2020/06/06.
+//  Copyright © 2020 Michelle Lau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func addMemo(_ sender: UIButton) {
+    }
+    
+}
+
